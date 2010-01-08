93a9b8b35a4a454208c113d2339390577ac3e55f
robmckinnon <rob@theyworkforyou.co.nz>
2010-01-08 18:52:48 +0000

    set foreign keys on associations

--- lib/app/models/uk2010_constituency.rb
+++ lib/app/models/uk2010_constituency.rb
@@ -1,6 +1,6 @@
 class Uk2010Constituency < ActiveRecord::Base
 
-  belongs_to :country, :class_name => 'UkCountry'
-  belongs_to :region, :class_name => 'UkRegion'
+  belongs_to :country, :class_name => 'UkCountry', :foreign_key => 'uk_country_id'
+  belongs_to :region, :class_name => 'UkRegion', :foreign_key => 'uk_region_id'
 
 end
--- lib/app/models/uk_region.rb
+++ lib/app/models/uk_region.rb
@@ -1,6 +1,6 @@
 class UkRegion < ActiveRecord::Base
 
-  belongs_to :country, :class_name => 'UkCountry'
+  belongs_to :country, :class_name => 'UkCountry', :foreign_key => 'uk_country_id'
   has_many :constituencies, :class_name => 'Uk2010Constituency'
 
 end
